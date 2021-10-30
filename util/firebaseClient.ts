8d441e101ff79712e46c3a72d92d4e8d947f3ee0
nathanluu <nate1299@me.com>
2021-10-30 12:51:53 -0400

    Added firebase config files

--- util/firebaseClient.ts
+++ util/firebaseClient.ts
@@ -4,7 +4,6 @@ import {FirebaseError} from "@firebase/util";
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_apiKey,
     authDomain: process.env.NEXT_PUBLIC_authDomain,
-    databaseURL: process.env.NEXT_PUBLIC_databaseURL,
     projectId: process.env.NEXT_PUBLIC_projectId,
     storageBucket: process.env.NEXT_PUBLIC_storageBucket,
     messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
