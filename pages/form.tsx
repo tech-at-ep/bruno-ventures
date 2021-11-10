dc367f6c56e32f0629e228db9ac4a44e53196e33
LukePrimis <luke_primis@brown.edu>
2021-11-10 01:57:38 -0500

    started adding background vector images

---
+++ pages/form.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/Form.module.css'
+
+export default function Form() {
+    return (
+        <div>
+            <img className={`${styles.top_left_vector}  ${styles.noselect} ${styles.nodrag}`} src="/form_vector_1.svg" />
+            <img className={`${styles.bottom_right_vector} ${styles.noselect} ${styles.nodrag}`} src="/form_vector_2.svg" />
+            <div className={`${styles.container}`}>
+                <h1>TODO: For form page</h1>
+            
+            </div>
+        </div>
+        
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>TODO</h1>
+    <div>
+      <Head>
+        <title>Test</title>
+      </Head>
+
+      <main>
+        <p>hello</p>
+      </main>
     </div>
   )
 }
