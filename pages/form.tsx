ded9455f878ba9469c11ab1db2c63e925507e6bd
LukePrimis <luke_primis@brown.edu>
2021-11-12 18:53:01 -0500

    more styling stuff

--- pages/form.tsx
+++ pages/form.tsx
@@ -1,15 +1,28 @@
 import styles from '../styles/Form.module.css'
 
+function Card() {
+    return (
+        <div className={styles.form_card}>
+            <div className={styles.form_card_header}>List Your Startup</div>
+        </div>
+    )
+}
+
 export default function Form() {
     return (
         <div>
             <img className={`${styles.top_left_vector}  ${styles.noselect} ${styles.nodrag}`} src="/form_vector_1.svg" />
             <img className={`${styles.bottom_right_vector} ${styles.noselect} ${styles.nodrag}`} src="/form_vector_2.svg" />
-            <div className={`${styles.container}`}>
-                <h1>TODO: For form page</h1>
-            
+            <div className={styles.form_title}>
+                <div>Welcome to</div>
+                <div>Bruno Ventures.</div>
             </div>
-        </div>
+            <div className={`${styles.container}`}>
+                {/* <h1>TODO: For form page</h1> */}
+                <Card />            
+            </div>
+       </div>
         
     )
 }
+
