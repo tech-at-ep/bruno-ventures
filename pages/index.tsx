67040e146007aa41873ab9ffd7c76c16c3d348d4
nathanluu <nate1299@me.com>
2021-11-14 22:42:50 -0500

    Added FB Auth demo to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import styles from '../styles/Home.module.css'
+import {useAuth} from "../util/firebaseAuthHelpers";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>TODO</h1>
-    </div>
-  )
+    const {isAuthenticated, user, signInWithGoogle, signOut} = useAuth();
+
+    return (
+        <div className={styles.container}>
+            {isAuthenticated ? <div>
+                <h1>Signed in as {user?.email}</h1>
+                <button onClick={signOut}
+                        className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Sign out
+                </button>
+            </div> : <div>
+                <h1>Signed out:</h1>
+                <button onClick={signInWithGoogle}
+                        className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Sign in with Google
+                </button>
+            </div>}
+
+        </div>
+    )
 }
