7c22db0168ccc619c48cc710289a432ce4dfaac7
jennjwang <jennifer123wang@gmail.com>
2021-11-14 20:31:51 -0500

    fixed firebase issues

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
 import '../styles/globals.css'
 import type {AppProps} from 'next/app'
-// import {FirebaseAuthProvider, useFirebaseAuth} from "../util/firebaseAuthHelpers"
-// import Head from "next/head";
+import {FirebaseAuthProvider, useFirebaseAuth} from "../util/firebaseAuthHelpers"
+import Head from "next/head";
 
 function MyApp({Component, pageProps}: AppProps) {
-    // const firebaseAuthState = useFirebaseAuth();
+    const firebaseAuthState = useFirebaseAuth();
 
     // Waits until the session is loaded before loading the page
-    // if (firebaseAuthState.isLoading) return null
+    if (firebaseAuthState.isLoading) return null
 
-    // return (<FirebaseAuthProvider value={firebaseAuthState}>
-    //     <Head>
-    //         <title>🐻 Bruno Ventures</title>
-    //     </Head>
-    //     <Component {...pageProps} />
-    // </FirebaseAuthProvider>)
-    return <Component {...pageProps} />
+    return (<FirebaseAuthProvider value={firebaseAuthState}>
+        <Head>
+            <title>🐻 Bruno Ventures</title>
+        </Head>
+        <Component {...pageProps} />
+    </FirebaseAuthProvider>)
+    // return <Component {...pageProps} />
 }
 
 export default MyApp
--- util/firebaseClient.ts
+++ util/firebaseClient.ts
@@ -1,3 +1,4 @@
+
 import firebase from 'firebase';
 import {FirebaseError} from "@firebase/util";
 
