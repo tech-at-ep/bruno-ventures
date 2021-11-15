4490acf7eceef2f0c07c3df7b8349e9c5e50e9a8
nathanluu <nate1299@me.com>
2021-11-14 22:46:14 -0500

    Updated readme.md

--- util/firebaseAuthHelpers.ts
+++ util/firebaseAuthHelpers.ts
@@ -1,5 +1,5 @@
 import {createContext, useContext, useEffect, useState} from 'react';
-import firebaseApp from "./firebaseClient";
+import firebaseApp from "./firebaseApp";
 import {
     getAuth,
     GoogleAuthProvider,
