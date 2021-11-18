ceefd02b3d7bf427b2550f514fe460557feaa6c0
michaelx78 <michael_w_xu@brown.edu>
2021-11-18 16:32:43 -0500

    Updating env

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import styles from '../styles/Home.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.title}>Name of Company Here</div> 
-      <div className={styles.logo}>Logo Here</div>
+    <div>
+      <Head>
+        <title>Test</title>
+      </Head>
+
+      <main>
+        <p>hello</p>
+      </main>
     </div>
   )
 }
