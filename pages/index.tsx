6208716f8da7449cd0b296acdc5d1a5d46e3f964
jfrausto7 <jacob_frausto@brown.edu>
2021-11-18 21:18:42 -0500

    removed firebase demo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,9 @@
 import styles from '../styles/Home.module.css'
-import {useAuth} from "../util/firebaseAuthHelpers";
-import firebase from 'firebase';
 
 export default function Home() {
-    const {isAuthenticated, user, signInWithGoogle, signOut} = useAuth();
 
     return (
-        <div className={styles.container}>
-            {isAuthenticated ? <div>
-                <h1>Signed in as {user?.email}</h1>
-                <button onClick={signOut}
-                        className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Sign out
-                </button>
-            </div> : <div>
-                <h1>Signed out:</h1>
-                <button onClick={signInWithGoogle}
-                        className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Sign in with Google
-                </button>
-            </div>}
-
+        <div>
         </div>
     )
 }
