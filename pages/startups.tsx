66fbc55ff087b46068b6782b79522b0df78005e0
unknown <abigail-powell@brown.edu>
2021-11-21 17:34:38 -0500

    Added async function

--- pages/startups.tsx
+++ pages/startups.tsx
@@ -13,46 +13,49 @@ export default function Startups() {
     useEffect(() => {
       const db = getFirestore();
       const startups = collection(db, "apps");
+      async function beginSnapStartup() {
       const snapStartup = await getDocs(startups);
       //For each startup, displays a unique button that provides more
       //  information about the startup when clicked on
-      snapStartup.forEach(doc => {
-        let btn = doc.createElement("button"); //doc or document?
-        var sData = doc.data();
-        var sName = sData.name;
-        btn.innerHTML = sName;
-        //When the button is clicked, blur the background and show the startup info
-        btn.onClick = function(sData){
-          //Function title: showStartupInfo
-          // Show overlay
-          var overlayBox = doc.createElement('div');
-          overlayBox.className = 'styles.overlay';
-          //doc.body.appendChild(overlayBox);
-          // Design Startup box
-          var popupBox = doc.createElement('Popup');
-          popupBox.anchor = anchor.current;
-          popupBox.popupClass = 'styles.popup-content';
-          // Add startup info
-          var startupName = sData.name;
-          var sIndustry = sData.industry;
-          var sContact = sData.email;
-          var sWeb = sData.website;
-          var sMis = sData.mission;
-          var startupInfo = `\t\tIndustry: \tContact: \tWebsite:\n\t\t${sIndustry}\t${sContact}\t${sWeb}\n\n\n\nMission: \n${sMis}`;
-          var popContent = doc.createTextNode(startupInfo);
-          var nameTag = doc.createElement('span');
-          nameTag.className = 'styles.dot';
-          nameTagData = doc.createTextNode(startupName);
-          popupBox.classList.toggle("show");
-          //Put tags together
-          nameTag.appendChild(nameTagData);
-          popupBox.appendChild(nameTag);
-          popupBox.appendChild(popContent);
-          overlayBox.appendChild(popupBox);
-          doc.body.appendChild(overlayBox);
-        }
-        doc.body.appendChild(btn); //doc or document?
-      });
+        snapStartup.forEach(doc => {
+          let btn = doc.createElement("button"); //doc or document?
+          var sData = doc.data();
+          var sName = sData.name;
+          btn.innerHTML = sName;
+          //When the button is clicked, blur the background and show the startup info
+          btn.onClick = function(sData){
+            //Function title: showStartupInfo
+            // Show overlay
+            var overlayBox = doc.createElement('div');
+            overlayBox.className = 'styles.overlay';
+            //doc.body.appendChild(overlayBox);
+            // Design Startup box
+            var popupBox = doc.createElement('Popup');
+            popupBox.anchor = anchor.current;
+            popupBox.popupClass = 'styles.popup-content';
+            // Add startup info
+            var startupName = sData.name;
+            var sIndustry = sData.industry;
+            var sContact = sData.email;
+            var sWeb = sData.website;
+            var sMis = sData.mission;
+            var startupInfo = `\t\tIndustry: \tContact: \tWebsite:\n\t\t${sIndustry}\t${sContact}\t${sWeb}\n\n\n\nMission: \n${sMis}`;
+            var popContent = doc.createTextNode(startupInfo);
+            var nameTag = doc.createElement('span');
+            nameTag.className = 'styles.dot';
+            nameTagData = doc.createTextNode(startupName);
+            popupBox.classList.toggle("show");
+            //Put tags together
+            nameTag.appendChild(nameTagData);
+            popupBox.appendChild(nameTag);
+            popupBox.appendChild(popContent);
+            overlayBox.appendChild(popupBox);
+            doc.body.appendChild(overlayBox);
+          }
+          doc.body.appendChild(btn); //doc or document?
+        });
+      };
+      beginSnapStartup();
     }, []);
     return (
         <div className={styles.container}>
