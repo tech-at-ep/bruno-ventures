850b99034dd4635f48488bbfd212a7838b185194
michaelx78 <michael_w_xu@brown.edu>
2021-11-21 20:43:10 -0500

    Added plain info for company (industry, year, etc.)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import styles from '../styles/Home.module.css'
 import Head from "next/head";
 import opensea from '../images/opensea_logo.png'
+import Dropdown from "../components/Dropdown"
+
+const imagePath = '../images/opensea_logo.png'
+// import image from imagePath
 
 function Header() {
     return (
@@ -11,7 +15,7 @@ function Header() {
 }
 
 function Logo() {
-    return <img src={opensea.src} />
+    return <img src={imagePath} width="66" height="65"/>
 }
 
 export default function Startups() {
@@ -20,6 +24,24 @@ export default function Startups() {
             <Header />
             <div className={styles.logo}>
             <Logo />
+            </div>
+            <div className={styles.dropdown}>
+                <div className={styles.description}>
+                <h2>Industry</h2>
+                <p>Blockchain</p>
+                </div>
+                <div className={styles.description}>
+                <h2>Year</h2>
+                <p>2019</p>
+                </div>
+                <div className={styles.description}>
+                <h2>Contact</h2>
+                <p>hello@opensea.io</p>
+                </div>
+                <div className={styles.description}>
+                <h2>Website</h2>
+                <p><a href="https://opensea.io/">opensea.io</a></p>
+                </div>
             </div>
             <div className={styles.description}>
             Mission Statement: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id venenatis diam. Pellentesque enim enim, fermentum vel nisi quis, facilisis dapibus turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
