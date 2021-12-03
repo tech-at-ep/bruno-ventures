bf4684185b8d10bcd91a743b92965d32df4f875c
michaelx78 <michael_w_xu@brown.edu>
2021-12-03 18:47:17 -0500

    Switch to tailwind w/ help from nathan

--- pages/[startupSlug].tsx
+++ pages/[startupSlug].tsx
@@ -1,16 +1,62 @@
-import styles from '../styles/Home.module.css'
-import {useRouter} from "next/router";
-import {useAuth} from "../util/firebaseAuthHelpers";
+import { useRouter } from "next/router";
+import { useAuth } from "../util/firebaseAuthHelpers";
 
 export default function Startup() {
     const router = useRouter();
-    const {startupSlug} = router.query;
+    const { startupSlug } = router.query;
 
-    const {user} = useAuth();
+    const { user } = useAuth();
 
     return (
-        <div className={styles.container}>
-            <h1>TODO: Startup page for {startupSlug}</h1>
+        <div className="">
+            <div className="h-96 bg-blue-500" />
+            <div className="max-w-3xl mx-auto -mt-32">
+                <h1 className="text-6xl font-bold text-white">
+                    At a glance: OpenSea
+                </h1>
+
+                <div className="flex justify-start items-center space-x-8 mb-8">
+
+                    <div>
+                        <div className="font-bold">
+                            Industry
+                        </div>
+                        Blockchain
+                    </div>
+
+                    <div>
+                        <div className="font-bold">
+                            Year
+                        </div>
+                        2019
+                    </div>
+
+                    <div>
+                        <div className="font-bold">
+                            Contact
+                        </div>
+                        hello@opensea.io
+                    </div>
+
+                    <div>
+                        <div className="font-bold">
+                            Website
+                        </div>
+                        <a href="https://opensea.io" className="hover:text-blue-600 text-blue-500 focus:underline">opensea.io</a>
+                    </div>
+
+                </div>
+
+
+                <div>
+                    <span className="font-bold">Mission Statement:</span> We're building tools that allow consumers to trade their assets freely, creators to launch new digital works, and developers to build rich, integrated marketplaces for their digital assets.
+                </div>
+
+                <div className="mt-4">
+                    <span className="font-bold">Description:</span> OpenSea is a peer-to-peer marketplace for crypto collectibles and non-fungible tokens. It includes collectibles, gaming items, and other virtual goods backed by a blockchain. On OpenSea, anyone can buy or sell these items through a smart contract.
+                </div>
+
+            </div>
         </div>
     )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,56 +1,20 @@
-import styles from '../styles/Home.module.css'
+import '../styles/globals.css'
+import type {AppProps} from 'next/app'
+import {FirebaseAuthProvider, useFirebaseAuth} from "../util/firebaseAuthHelpers"
 import Head from "next/head";
-import opensea from '../images/opensea_logo.png'
-import Dropdown from "../components/Dropdown"
 
-const imagePath = '../images/opensea_logo.png'
-// import image from imagePath
 
-function Header() {
-    return (
-        <div className={styles.title}>
-            Test Title
-        </div>
-    )
+
+
+function MyApp({Component, pageProps}: AppProps) {
+    const firebaseAuthState = useFirebaseAuth();
+    
+    return (<FirebaseAuthProvider value={firebaseAuthState}>
+        <Head>
+            <title>🐻 Bruno Ventures</title>
+        </Head>
+        <Component {...pageProps} />
+    </FirebaseAuthProvider>)
 }
 
-function Logo() {
-    return <img src={opensea.src} width="66" height="65"/>
-}
-
-export default function Startups() {
-    return (
-        <div className={styles.container}>
-            <div className={styles.rectangle}>
-            <Header />
-            </div>
-            <div className={styles.dropdown}>
-            <div className={styles.logo}>
-            <Logo />
-            </div>
-                <div className={styles.description}>
-                <h3>Industry</h3>
-                <p>Blockchain</p>
-                </div>
-                <div className={styles.description}>
-                <h3>Year</h3>
-                <p>2019</p>
-                </div>
-                <div className={styles.description}>
-                <h3>Contact</h3>
-                <p>hello@opensea.io</p>
-                </div>
-                <div className={styles.description}>
-                <h3>Website</h3>
-                <p><a href="https://opensea.io/">opensea.io</a></p>
-                </div>
-            </div>
-            <b>Mission Statement:</b> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id venenatis diam. Pellentesque enim enim, fermentum vel nisi quis, facilisis dapibus turpis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-
-Description: Mauris ultricies eu nibh at efficitur. Ut tortor metus, pellentesque eu turpis tincidunt, ultricies semper dui. Sed tincidunt sit amet enim quis facilisis. Sed a volutpat augue. Fusce gravida sapien risus, vitae consectetur magna maximus at. Aliquam a augue tellus. Mauris eleifend, tortor a venenatis laoreet, justo nisi euismod libero, quis pulvinar arcu arcu non sapien. Donec pulvinar elit id eros varius convallis.
-            <div className={styles.description}>
-            
-            </div>
-        </div>
-    )
-}
+export default MyApp
