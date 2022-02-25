5e29f02f18216673e0c28b1f25b9119f6eb84c5e
“John1114” <john_rytel@brown.edu>
2022-02-24 21:27:04 -0500

    updated divs and some components removed

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,11 +5,16 @@ import { useState, useEffect } from "react";
 
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import AboutStatement from "../components/AboutStatement";
+import AboutStatement2 from "../components/AboutStatement2";
 import Ourteam from "../components/Ourteam";
 import LegalNotice from "../components/LegalNotice";
 
 import logo from "../assets/logo.jpeg";
+import Aaron from "../images/headshots/Aaron.jpeg";
+import Jacob from "../images/headshots/Jacob_Headshot.jpeg";
+import Michael from "../images/headshots/Michael_headshot.png";
+import Nathan from "../images/headshots/nathan.jpeg";
+import Placeholder from "../images/headshots/placeholder.jpeg";
 
 export default function Home() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -32,21 +37,60 @@ export default function Home() {
 
   setTimeout(StopLoading, 2000);
 
+  const headlineStyle = {
+  color: 'rgb(239, 68, 68)'
+  };
+  const cardStyle = {
+    // height: '200px',
+    // width: '150px'
+  };
+  const nameStyle = {
+    // fontSize: '30px',
+  }
+  const titleStyle = {
+    // fontWeight: 'normal',
+  }
+  const imgStyle= {
+    //   height: '130px',
+    // width: '150px'
+  }
+
   return (
     <div>
       {loading && <SplashScreen fading={fading} />}
       <div className="box-border">
         <div className="flex flex-col">
           <Navbar />
-          <AboutStatement />
+          <div className="p-44 items-center">
+            <div className="headline__headline text-2xl font-bold text-center text-blue-800 lg:mx-auto lg:w-6/6 lg:text-8xl lg:text-gray-90 " style={headlineStyle}>
+              ABOUT BRUNO VENTURES
+            </div>
+          </div>
         </div>
-        <div className="relative top-0">
-          <Ourteam />
+        <div className="flex flex-row">
+          <div className="text-center" style={{width: '50%'}}>
+            <p className="p-4 text-4xl text-center">
+              Mission Statement:
+            </p>
+            <p className="p-4 text-2xl text-center">
+              For five decades we have partnered with intrepid founders to build iconic companies that made history. Today, Kleiner Perkins continues that legacy, investing in founders with bold ideas that span industries and continents, partnering with them from inception to IPO and beyond to maximize the potential of their ideas… and make history.
+            </p>
+          </div>
+          <div className="text-center" style={{width: '50%'}}>
+            <p className="p-4 text-4xl text-center">
+              Our Team:
+            </p>
+            <p> Team Members</p>                      
+          </div>
         </div>
-        <div className="relative top-0">
-          <LegalNotice />
-        </div>
-
+        <div className="text-center p-20">
+            <p className="p-4 text-4xl text-center">
+              Legal Language — TO BE REVIEWED WITH LIZ MALONE:
+            </p>
+            <p className="p-4 text-2xl text-center">
+              The sole purpose of this website is to recognize the existing work of Brown University student ventures. Furthermore, Brown EP and the Nelson Center for Entrepreneurship are not liable for any unwarranted copying of current ventures.
+            </p>
+          </div>
         <div className="relative top-0 h-2 my-24">
           <Footer logo={logo} />
         </div>
